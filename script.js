ef60da7adabb01c2f4db4bc2e9baccab4f7984f7
Iyyappan Ganesan <133621882+iyyappan07@users.noreply.github.com>
2026-03-17 17:19:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ let row = `<tr>
 table.innerHTML += row;
 
 });
+
+function addStudent(){
+
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+
+if(name === "" || email === ""){
+alert("Please enter name and email");
+return;
+}
+
+let id = students.length + 1;
+
+let student = { id, name, email };
+
+students.push(student);
+
+let row = `<tr>
+<td>${student.id}</td>
+<td>${student.name}</td>
+<td>${student.email}</td>
+</tr>`;
+
+document.getElementById("studentTable").innerHTML += row;
+
+document.getElementById("name").value = "";
+document.getElementById("email").value = "";
+
+}
