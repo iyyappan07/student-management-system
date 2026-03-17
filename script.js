db8f6ae43da6ff5851a239d124977d806205b94f
Iyyappan Ganesan <133621882+iyyappan07@users.noreply.github.com>
2026-03-17 17:14:29 +0530

    Implement student data display in table
    Added student data and populated the student table.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Student Management System Loaded");
+const students = [
+{ id: 1, name: "Arun", email: "arun@email.com" },
+{ id: 2, name: "Vijay", email: "vijay@email.com" },
+{ id: 3, name: "Rahul", email: "rahul@email.com" }
+];
+
+const table = document.getElementById("studentTable");
+
+students.forEach(student => {
+
+let row = `<tr>
+<td>${student.id}</td>
+<td>${student.name}</td>
+<td>${student.email}</td>
+</tr>`;
+
+table.innerHTML += row;
+
+});
